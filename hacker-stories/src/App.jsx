870fe765b9b2bcb322840124ef9e0dce833e8c78
Kevin Karanja <kevinkaranja098@gmail.com>
2024-02-29 10:53:44 +0300

    using props from main app copponent to its child

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,58 +1,27 @@
 //import * as React from "react";
 
-const list = [
-  {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: 'Jordan Walke',
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-  },
-  {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: 'Dan Abramov, Andrew Clark',
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-  },
-  ];
 
-  function List() {
-    return (
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-    </ul>
-    );
-  }  
 
-  const Search = () => {
-    const handleChange = (event) => {
-      console.log(event);
-      console.log(event.target.value);
-    };
 
-    return(
-      <div>
-        <lebel htmlFor="search">Search:</lebel>
-        <input id="search" type="text" onChange={handleChange} />
-      </div>
-    );
-  }  
-
-function App() {
+const App=() => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ]
   
   return(
     <div>
@@ -62,13 +31,42 @@ function App() {
 
       <hr />
 
-      <List />
+      <List list={stories} />
       
 
     </div>
   )
-}
+  }
 
+  const List = (prop) => (
+    <ul>
+      {prop.list.map((item) => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+      ))}
+  </ul>
+  );
+
+
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  };
+
+  return(
+    <div>
+      <lebel htmlFor="search">Search:</lebel>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+}  
 
 
 export default App
